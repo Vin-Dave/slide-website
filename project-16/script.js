76db25d3e483c42ff6fc5f86b85ed9e6b42e59b0
Vin-Dave <126113143+Vin-Dave@users.noreply.github.com>
2023-05-02 19:15:59 +0200

    Update script.js

--- project-16/script.js
+++ project-16/script.js
@@ -54,3 +54,68 @@ function updateFilters() {
   completedBtn.innerHTML = `Completed (${completedTasks.length})`;
   uncompletedBtn.innerHTML = `Uncompleted (${uncompletedTasks.length})`;
 }
+
+// Filter tasks
+function filterTasks(filter) {
+  tasksList.querySelectorAll(".task-item").forEach((taskItem) => {
+    taskItem.style.display = "flex";
+    switch (filter) {
+      case "completed":
+        if (!taskItem.classList.contains("complete")) {
+          taskItem.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (taskItem.classList.contains("complete")) {
+          taskItem.style.display = "none";
+        }
+        break;
+      default:
+        break;
+    }
+  });
+  filters.querySelectorAll(".filter-btn").forEach((filterBtn) => {
+    filterBtn.classList.remove("active");
+  });
+  filters.querySelector(`#${filter}-btn`).classList.add("active");
+}
+
+function toggleTaskCompletion(e) {
+  if (!e.target.classList.contains("complete-btn")) return;
+  const taskItem = e.target.closest(".task-item");
+  const taskId = parseInt(taskItem.getAttribute("data-id"));
+  const taskIndex = tasks.findIndex((task) => task.id === taskId);
+  tasks[taskIndex].completed = !tasks[taskIndex].completed;
+  taskItem.classList.toggle("complete");
+  if (tasks[taskIndex].completed) {
+    taskItem.classList.add("completed");
+  } else {
+    taskItem.classList.remove("completed");
+  }
+  renderTasks();
+}
+
+// Delete task
+function deleteTask(e) {
+  if (!e.target.classList.contains("delete-btn")) return;
+  const taskItem = e.target.closest(".task-item");
+  const taskId = parseInt(taskItem.getAttribute("data-id"));
+  tasks = tasks.filter((task) => task.id !== taskId);
+  taskItem.classList.add("deleted");
+  taskItem.addEventListener("animationend", () => {
+    taskItem.remove();
+    updateFilters();
+  });
+}
+
+// Event listeners
+form.addEventListener("submit", addTask);
+tasksList.addEventListener("click", toggleTaskCompletion);
+tasksList.addEventListener("click", deleteTask);
+filters.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("filter-btn")) return;
+  filterTasks(e.target.getAttribute("data-filter"));
+});
+
+// Initial render
+renderTasks();
